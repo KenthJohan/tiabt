2679158d5b3ccb4cdda11773266f1252774a621e
Johan Söderlind Åström <johan.soderlind.astrom@gmail.com>
2022-04-03 21:00:10 +0200

    update

--- software/src/main.c
+++ software/src/main.c
@@ -35,28 +35,32 @@
 // https://github.com/zephyrproject-rtos/zephyr-testing/blob/0f09360456592ecf0d0413352dc1ef5d23cd0364/include/drivers/spi.h#L252
 // https://github.com/zephyrproject-rtos/zephyr-testing/blob/2fc0ace3c2462cb33dc8c25e33c629cedba08ebb/boards/arm/rm1xx_dvk/rm1xx_dvk.dts#L60
 // https://github.com/zephyrproject-rtos/zephyr/blob/ee91cd5665b1590c9a8a9a992bb19d81feeeb461/boards/arm/bt610/bt610.dts#L268
-// struct spi_cs_control *ctrl = SPI_CS_CONTROL_PTR_DT(DT_NODELABEL(spi1), 2);
-static struct spi_config spi_cfg = 
+
+//struct spi_cs_control *ctrl = SPI_CS_CONTROL_PTR_DT(DT_NODELABEL(spi1), 2);
+//struct spi_dt_spec spi_spec1 = SPI_DT_SPEC_GET(DT_NODELABEL(spi1), SPI_WORD_SET(8) | SPI_MODE_GET(0), 1);
+
+struct spi_dt_spec spi_spec = 
 {
-	.operation = SPI_WORD_SET(8) | SPI_MODE_GET(0),
-	.frequency = 1*1000*1000,
-	//.cs = SPI_CS_CONTROL_PTR_DT(spi1, 1)
-	.cs = &(struct spi_cs_control) 
+	.bus = DEVICE_DT_GET(DT_NODELABEL(spi1)),
+	.config = 
 	{
-		//.gpio_dev = DEVICE_DT_GET(DT_NODELABEL(spi1_nss_pa4))
-		//.gpio = SPI_CS_GPIOS_DT_SPEC_GET(spi1)
-		.gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpioa)),
-		.delay = 1,
-		.gpio_pin = 4,
-		.gpio_dt_flags = GPIO_ACTIVE_LOW
+		.operation = SPI_WORD_SET(8) | SPI_MODE_GET(0),
+		.frequency = 1*1000*1000,
+		.cs = &(struct spi_cs_control) 
+		{
+			.gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpioa)),
+			.delay = 1,
+			.gpio_pin = 4,
+			.gpio_dt_flags = GPIO_ACTIVE_LOW
+		}
 	}
 };
 
 
-static struct device const * dev_spi1 = DEVICE_DT_GET(DT_NODELABEL(spi1));
-static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_NODELABEL(blue_led_1), gpios);
-static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(DT_NODELABEL(green_led_2), gpios);
-static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(DT_NODELABEL(green_led_3), gpios);
+//static struct device const * dev_spi1 = DEVICE_DT_GET(DT_NODELABEL(spi1));
+//static const struct gpio_dt_spec led1 = GPIO_DT_SPEC_GET(DT_NODELABEL(blue_led_1), gpios);
+//static const struct gpio_dt_spec led2 = GPIO_DT_SPEC_GET(DT_NODELABEL(green_led_2), gpios);
+//static const struct gpio_dt_spec led3 = GPIO_DT_SPEC_GET(DT_NODELABEL(green_led_3), gpios);
 
 
 
@@ -70,7 +74,7 @@ static void set8(uint8_t reg, uint8_t value)
 	struct spi_buf buf_rx[] = {{.buf = &rx,.len = sizeof(rx)}};
 	struct spi_buf_set tx_buf = {.buffers = buf_tx, .count = 1};
 	struct spi_buf_set rx_buf = {.buffers = buf_rx, .count = 1};
-	spi_transceive(dev_spi1, &spi_cfg, &tx_buf, &rx_buf);
+	spi_transceive_dt(&spi_spec, &tx_buf, &rx_buf);
 }
 
 
@@ -84,7 +88,7 @@ static uint8_t get8(uint8_t reg)
 	struct spi_buf_set rx_buf = {.buffers = buf_rx, .count = 1};
 	tx[0] = MCP356X_COMMAND_BYTE(MCP356X_DEVICE_ADR, reg, MCP356X_CMD_INC_READ);
 	tx[1] = 0; // Need to write in order to read. Exchange 0 for reading data.
-	spi_transceive(dev_spi1, &spi_cfg, &tx_buf, &rx_buf);
+	spi_transceive_dt(&spi_spec, &tx_buf, &rx_buf);
 	return rx[1];
 }
 
@@ -100,8 +104,8 @@ static uint32_t get24(uint8_t reg)
 	tx[1] = 0; // Need to write in order to read. Exchange 0 for reading data.
 	tx[2] = 0; // Need to write in order to read. Exchange 0 for reading data.
 	tx[3] = 0; // Need to write in order to read. Exchange 0 for reading data.
-	spi_transceive(dev_spi1, &spi_cfg, &tx_buf, &rx_buf);
-	uint32_t v = (rx[1] << 16) | (rx[2] << 8) |  (rx[3] << 0);
+	spi_transceive_dt(&spi_spec, &tx_buf, &rx_buf);
+	uint32_t v = (rx[1] << 16) | (rx[2] << 8) | (rx[3] << 0);
 	return v;
 }
 
@@ -118,8 +122,8 @@ static uint32_t get32(uint8_t reg)
 	tx[2] = 0; // Need to write in order to read. Exchange 0 for reading data.
 	tx[3] = 0; // Need to write in order to read. Exchange 0 for reading data.
 	tx[4] = 0; // Need to write in order to read. Exchange 0 for reading data.
-	spi_transceive(dev_spi1, &spi_cfg, &tx_buf, &rx_buf);
-	uint32_t v = (rx[1] << 24) | (rx[2] << 16) | (rx[3] << 8) |  (rx[4] << 0);
+	spi_transceive_dt(&spi_spec, &tx_buf, &rx_buf);
+	uint32_t v = (rx[1] << 24) | (rx[2] << 16) | (rx[3] << 8) | (rx[4] << 0);
 	return v;
 }
 
@@ -138,7 +142,7 @@ static void set24(uint8_t reg, uint32_t value)
 	struct spi_buf buf_rx[] = {{.buf = &rx,.len = sizeof(rx)}};
 	struct spi_buf_set tx_buf = {.buffers = buf_tx, .count = 1};
 	struct spi_buf_set rx_buf = {.buffers = buf_rx, .count = 1};
-	spi_transceive(dev_spi1, &spi_cfg, &tx_buf, &rx_buf);
+	spi_transceive_dt(&spi_spec, &tx_buf, &rx_buf);
 }
 
 
@@ -217,9 +221,9 @@ void main(void)
 	
 	init_adc();
 
-	gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
-	gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
+	//gpio_pin_configure_dt(&led1, GPIO_OUTPUT_ACTIVE);
+	//gpio_pin_configure_dt(&led2, GPIO_OUTPUT_ACTIVE);
+	//gpio_pin_configure_dt(&led3, GPIO_OUTPUT_ACTIVE);
 	/*
 	gpio_pin_toggle_dt(&led1);
 	gpio_pin_toggle_dt(&led2);

9569b80a6dfcefc6b3f02fc6d28c399df2056cb2
Johan Söderlind Åström <kenth.johan.work@gmail.com>
2022-04-04 13:23:13 +0200

    update

--- software/src/adc.h
+++ software/src/adc.h
@@ -4,18 +4,75 @@
 // https://github.com/MikroElektronika/mikrosdk_click_v2/tree/master/clicks/adc9
 // https://github.com/jspark311/MCP356x-Breakout/blob/6cc57254ad991eba59b2607941b86dc148f06eaf/Arduino/src/MCP356x.cpp
 
+
+
+
+
+
+/*****************************************************************
+6.2.1 COMMAND BYTE STRUCTURE
+The COMMAND byte fully defines the command that
+will be executed by the part. This byte is divided into
+three parts: the device address bits (CMD[7:6]), the
+command address bits (CMD[5:2]) and the
+command-type bits (CMD[1:0]).
+*****************************************************************/
 #define MCP356X_COMMAND_BYTE(addr, cmd, type) (((addr)<<6) | ((cmd)<<2) | ((type)<<0))
 
-#define MCP356X_DEVICE_ADR                   0x01
+/*****************************************************************
+6.2.2 DEVICE ADDRESS BITS (CMD[7:6])
+The SPI interface of the MCP3561/2/4 devices is
+addressable, which means that multiple devices can
+communicate on the same SPI bus with only one Chip
+Select line for all devices. Each device communication
+starts by a CS falling edge, followed by the clocking of
+the device address (CMD[7:6]). Each device contains
+an internal device address which the device can
+respond to.
+This address is coded on two bits, so four possible
+addresses are available. Device address is hard-coded
+within the device and should be determined when
+ordering the device. The device address is part of the
+device markings to avoid potential confusion (see
+Sections 9.1 “Package Marking Information(1)”).
+When the CMD[7:6] bits match the device address, the
+communication will proceed and the part will execute
+the commands defined in the control byte and its
+subsequent data bytes.
+When the CMD[7:6] bits do not correspond to the
+address hard-coded in the device, the command is
+ignored. In this case, the SDO output will become
+high-impedance, which prevents bus contention errors
+when multiple devices are connected on the same SPI
+bus (see Figure 6-2). The user has to exit from this
+communication through a CS rising edge to be able to
+launch another command.
+*****************************************************************/
+#define MCP356X_DEVICE_ADR                   0x01 // CMD[7:6]
 
-
-
+/*****************************************************************
+6.2.4 COMMAND-TYPE BITS (CMD[1:0])
+The last two bits of the COMMAND register byte define
+the command type. These bits are an extension of the
+typical read/write bits present in most SPI communica-
+tion protocols. The two bits define four possible
+command types: Incremental Write, Incremental Read,
+Static Read and Fast command. Changing the com-
+mand type within the same communication (while CS is
+logic low) is not possible. The communication has to be
+stopped (CS rising edge) and restarted (CS falling
+edge) to change its command type.
+*****************************************************************/
 #define MCP356X_CMD_DONT_CARE                0x00
 #define MCP356X_CMD_STAT_READ                0x01
 #define MCP356X_CMD_INC_WRITE                0x02
 #define MCP356X_CMD_INC_READ                 0x03
 
-
+/*****************************************************************
+8.0 INTERNAL REGISTERS
+The MCP3561/2/4 devices have a total of 16 internal
+registers made of volatile memory.
+*****************************************************************/
 #define MCP356X_REG_ADC_DATA                 0x00
 #define MCP356X_REG_CFG_0                    0x01
 #define MCP356X_REG_CFG_1                    0x02
@@ -56,31 +113,47 @@ static char const * MCP356X_REG_tostring(int a)
 }
 
 
+
+
+/*****************************************************************
+8.2 CONFIG0 Register
+ADC Operating mode, Master Clock mode and Input Bias Current
+Source mode
+******************************************************************/
+// Full Shutdown Mode Enable
 #define MCP356X_CFG_0_VREF_SEL_0             0x00
 #define MCP356X_CFG_0_VREF_SEL_1             0x40
 #define MCP356X_CFG_0_VREF_SEL_2             0x80
 #define MCP356X_CFG_0_VREF_SEL_3             0xC0
-
+// Clock Selection
 #define MCP356X_CFG_0_CLK_SEL_0              0x00
 #define MCP356X_CFG_0_CLK_SEL_1              0x10
 #define MCP356X_CFG_0_CLK_SEL_2              0x20
 #define MCP356X_CFG_0_CLK_SEL_3              0x30
-
+// Current Source/Sink Selection Bits for Sensor Bias (source on VIN+/sink on VIN-)
 #define MCP356X_CFG_0_CS_SEL_0               0x00
 #define MCP356X_CFG_0_CS_SEL_1               0x04
 #define MCP356X_CFG_0_CS_SEL_2               0x08
 #define MCP356X_CFG_0_CS_SEL_3               0x0C
-
+// ADC Operating Mode Selection
 #define MCP356X_CFG_0_MODE_SHD_DEF           0x00
 #define MCP356X_CFG_0_MODE_SHD               0x01
 #define MCP356X_CFG_0_MODE_STANDBY           0x02
 #define MCP356X_CFG_0_MODE_CONV              0x03
 
+
+
+
+/******************************************************************
+8.3 CONFIG1 Register
+Prescale and OSR settings
+******************************************************************/
+// Prescaler Value Selection for AMCLK
 #define MCP356X_CFG_1_PRE_1                  0x00
 #define MCP356X_CFG_1_PRE_2                  0x40
 #define MCP356X_CFG_1_PRE_4                  0x80
 #define MCP356X_CFG_1_PRE_8                  0xC0
-
+// Oversampling Ratio for Delta-Sigma A/D Conversion
 #define MCP356X_CFG_1_OSR_98304              0x3C
 #define MCP356X_CFG_1_OSR_81920              0x38
 #define MCP356X_CFG_1_OSR_49152              0x34
@@ -97,17 +170,27 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_CFG_1_OSR_128                0x08
 #define MCP356X_CFG_1_OSR_64                 0x04
 #define MCP356X_CFG_1_OSR_32                 0x00
-
+// RESERVED[1:0]: Should always be set to ‘00’
 #define MCP356X_CFG_1_DITHER_MAX             0x03
 #define MCP356X_CFG_1_DITHER_MED             0x02
 #define MCP356X_CFG_1_DITHER_MIN             0x01
 #define MCP356X_CFG_1_DITHER_DEF             0x00
 
+
+
+
+
+/******************************************************************
+8.4 CONFIG2 Register
+ADC boost and gain settings, auto-zeroing settings for analog
+multiplexer, voltage reference and ADC
+******************************************************************/
+// ADC Bias Current Selection
 #define MCP356X_CFG_2_BOOST_X_2              0xC0
 #define MCP356X_CFG_2_BOOST_X_1              0x80
 #define MCP356X_CFG_2_BOOST_X_066            0x40
 #define MCP356X_CFG_2_BOOST_X_05             0x00
-
+// ADC Gain Selection
 #define MCP356X_CFG_2_GAIN_X_64              0x38
 #define MCP356X_CFG_2_GAIN_X_32              0x30
 #define MCP356X_CFG_2_GAIN_X_16              0x28
@@ -116,36 +199,54 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_CFG_2_GAIN_X_2               0x10
 #define MCP356X_CFG_2_GAIN_X_1               0x08
 #define MCP356X_CFG_2_GAIN_X_033             0x00
-
+// Auto-Zeroing MUX Setting
 #define MCP356X_CFG_2_AZ_MUX_EN              0x04
 #define MCP356X_CFG_2_AZ_MUX_DIS             0x00
 #define MCP356X_CFG_2_AZ_VREF_EN             0x02
 #define MCP356X_CFG_2_AZ_VREF_DIS            0x00
-
+// RESERVED[1:0]: Should always be equal to ‘11’
 #define MCP356X_CFG_2_AZ_FREQ_HIGH           0x01
 #define MCP356X_CFG_2_AZ_FREQ_LOW            0x00
 
+
+
+
+/******************************************************************
+8.5 CONFIG3 Register
+Conversion mode, data and CRC format settings; enable for CRC on
+communications, enable for digital offset and gain error calibrations
+*******************************************************************/
+// Conversion Mode Selection
 #define MCP356X_CFG_3_CONV_MODE_CONT         0xC0
 #define MCP356X_CFG_3_CONV_MODE_STANDBY      0x80
 #define MCP356X_CFG_3_CONV_MODE_SHD          0x40
 #define MCP356X_CFG_3_CONV_MODE_SHD0         0x00
-
+// ADC Output Data Format Selection
 #define MCP356X_CFG_3_DATA_FORMAT_CH_ADC     0x30
 #define MCP356X_CFG_3_DATA_FORMAT_SIGN_ADC   0x20
 #define MCP356X_CFG_3_DATA_FORMAT_LEFT_JUST  0x10
 #define MCP356X_CFG_3_DATA_FORMAT_DEF        0x00
-
+// CRC Checksum Format Selection on Read Communications
 #define MCP356X_CFG_3_CRC_FORMAT_32          0x08
 #define MCP356X_CFG_3_CRC_FORMAT_16          0x00
-
+// CRC Checksum Selection on Read Communications (it does not affect CRCCFG calculations)
 #define MCP356X_CFG_3_CRC_COM_EN             0x04
 #define MCP356X_CFG_3_CRC_COM_DIS            0x00
-
+// Enable Digital Offset Calibration
 #define MCP356X_CFG_3_CRC_OFF_CAL_EN         0x02
 #define MCP356X_CFG_3_CRC_OFF_CAL_DIS        0x00
+// Enable Digital Gain Calibration
 #define MCP356X_CFG_3_CRC_GAIN_CAL_EN        0x01
 #define MCP356X_CFG_3_CRC_GAIN_CAL_DIS       0x00
 
+
+
+
+/*******************************************************************
+8.6 IRQ Register
+IRQ Status bits and IRQ mode settings; enable for Fast commands and
+for conversion start pulse
+*******************************************************************/
 #define MCP356X_IRQ_MODE_MDAT                0x08
 #define MCP356X_IRQ_MODE_IRQ                 0x00
 #define MCP356X_IRQ_MODE_LOGIC_HIGH          0x04
@@ -155,6 +256,15 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_IRQ_STP_EN                   0x01
 #define MCP356X_IRQ_STP_DIS                  0x00
 
+
+
+
+
+/*******************************************************************
+8.7 MULTIPLEXER (MUX) REGISTER
+Analog multiplexer input selection (MUX mode only)
+*******************************************************************/
+// Bit 7-4 MUX_VIN+ Input Selection
 #define MCP356X_MUX_VIN_POS_NO_IN            0xF0 // 1111 0000 Internal VCM
 #define MCP356X_MUX_VIN_POS_VCM              0xE0 // 1110 0000 Internal Temperature Sensor Diode M (Temp Diode M)(1)
 #define MCP356X_MUX_VIN_POS_TEMP             0xD0 // 1101 0000 Internal Temperature Sensor Diode P (Temp Diode P)(1)
@@ -171,7 +281,7 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_MUX_VIN_POS_CH2              0x20 // 0010 0000
 #define MCP356X_MUX_VIN_POS_CH1              0x10 // 0001 0000
 #define MCP356X_MUX_VIN_POS_CH0              0x00 // 0000 0000
-
+// Bit 3-0 MUX_VIN- Input Selection
 #define MCP356X_MUX_VIN_NEG_NO_IN            0x0F // 0000 1111
 #define MCP356X_MUX_VIN_NEG_VCM              0x0E // 0000 1110 Internal Temperature Sensor Diode M (Temp Diode M)(1)
 #define MCP356X_MUX_VIN_NEG_TEMP             0x0D // 0000 1101 Internal Temperature Sensor Diode P (Temp Diode P)(1)
@@ -189,6 +299,11 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_MUX_VIN_NEG_CH1              0x01 // 0000 0001
 #define MCP356X_MUX_VIN_NEG_CH0              0x00 // 0000 0000
 
+
+
+
+// 8.8 SCAN Register
+// Bit 23-21 DLY[2:0]: Delay Time (TDLY_SCAN) Between Each Conversion During a Scan Cycle
 #define MCP356X_SCAN_DLY_DM_CLK_X_512        0x00E00000
 #define MCP356X_SCAN_DLY_DM_CLK_X_256        0x00C00000
 #define MCP356X_SCAN_DLY_DM_CLK_X_128        0x00A00000
@@ -197,10 +312,10 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_SCAN_DLY_DM_CLK_X_16         0x00400000
 #define MCP356X_SCAN_DLY_DM_CLK_X_8          0x00200000
 #define MCP356X_SCAN_DLY_NO_DELAY            0x00000000
-
+//Bit 19-16 Unimplemented: Read as ‘0’
 #define MCP356X_SCAN_PSAV_VREF_OFF           0x00100000
 #define MCP356X_SCAN_PSAV_VREF_ON            0x00000000
-
+// Bit 15-0 Scan Channel Selection (see Table 5-14 for a complete description)
 #define MCP356X_SCAN_OFFSET                  0x00008000
 #define MCP356X_SCAN_VREF                    0x00004000
 #define MCP356X_SCAN_AVDD                    0x00002000
@@ -218,6 +333,7 @@ static char const * MCP356X_REG_tostring(int a)
 #define MCP356X_SCAN_CH1                     0x00000002
 #define MCP356X_SCAN_CH0                     0x00000001
 
+// 8.9 TIMER Register
 #define MCP356X_TIMER_DLY_DMCLK_X_16777215   0x00FFFFFF
 #define MCP356X_TIMER_DLY_DMCLK_X_8388607    0x007FFFFF
 #define MCP356X_TIMER_DLY_DMCLK_X_4194303    0x003FFFFF
